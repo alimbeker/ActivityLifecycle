16ef0c2a980a59a88cde3ba37c02c2d2160b771b
Alimbekerov Amir <200103054@stu.sdu.edu.kz>
2023-10-05 16:29:26 +0600

    Make first activity

--- app/src/main/java/com/example/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.activitylifecycle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.activitylifecycle.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sendBtn.setOnClickListener {
+            val inputText = binding.timeForTimer.text.toString()
+            if (!inputText.isNullOrBlank()) {
+                val time = inputText.toInt()
+                val intent = Intent(this, TimerActivity::class.java)
+                intent.putExtra(Action.TIME.name, time)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Put time to timer", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+    }
+
+
+    enum class Action {
+        TIME
     }
 }
